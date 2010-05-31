c20d506c1e99c572cfed2538a25e58941323c01d
Stuart Morgan <stuart.morgan@alumni.case.edu>
2010-05-31 16:18:26 -0700

    Bug 520620: Change the Camino project's configuration names to the new-style Debug and Release. r=ardissone sr=pink

---
+++ scripts/copy-third-party-frameworks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Xcode doesn't understand that our third-party frameworks are built into different directories
+# than Camino is built into, so doing a normal copy phase doesn't work. Instead, we copy them
+# manually.
+# (This issue seems to be fixed in Snow Leopard, so we can hopefully eliminate this someday)
+
+echo Copying third-party frameworks to Camino build directory
+
+mkdir -p "${BUILT_PRODUCTS_DIR}/$WRAPPER_NAME/Contents/Frameworks/"
+
+BREAKPAD_DEST="${BUILT_PRODUCTS_DIR}/Breakpad.framework"
+rm -rf "$BREAKPAD_DEST"
+/Developer/Tools/CpMac -r "${SOURCE_ROOT}/google-breakpad/src/client/mac/build/${CONFIGURATION}/Breakpad.framework" "$BREAKPAD_DEST"
+
+GROWL_DEST="${BUILT_PRODUCTS_DIR}/Growl.framework"
+rm -rf "$GROWL_DEST"
+/Developer/Tools/CpMac -r "${SOURCE_ROOT}/growl/build/${CONFIGURATION}/Growl.framework" "$GROWL_DEST"
+
+SPARKLE_DEST="${BUILT_PRODUCTS_DIR}/Sparkle.framework"
+rm -rf "$SPARKLE_DEST"
+/Developer/Tools/CpMac -r "${SOURCE_ROOT}/sparkle/build/${CONFIGURATION}/Sparkle.framework" "$SPARKLE_DEST"
